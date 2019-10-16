cfa4ec79067913b35921c55a2be9ceacf0fc6644
chrisna2 <chrisna2@hanmail,net>
2019-10-16 17:51:59 +0900

    이동 평균선 백테스팅 모듈 완성

--- zipline/zipline_rolling_backtest.py
+++ zipline/zipline_rolling_backtest.py
@@ -23,6 +23,7 @@ data = data.tz_localize('UTC')
 def initialize(context):
     context.i = 0
     context.sym = symbol('AAPL')
+    context.hold = False
 
 
 def handle_data(context, data):
@@ -30,15 +31,22 @@ def handle_data(context, data):
     if context.i < 20:
         return
 
+    buy = False
+    sell = False
+
     ma5 = data.history(context.sym, 'price', 5, '1d').mean()
     ma20 = data.history(context.sym, 'price', 20, '1d').mean()
 
-    if ma5 > ma20:
-        order_target(context.sym, 1)
-    else:
-        order_target(context.sym, -1)
+    if ma5 > ma20 and context.hold == False:
+        order_target(context.sym, 100)
+        context.hold = True
+        buy = True
+    elif ma5 < ma20 and context.hold == True:
+        order_target(context.sym, -100)
+        context.hold = False
+        sell = True
 
-    record(AAPL=data.current(context.sym, "price"), ma5=ma5, ma20=ma20)
+    record(AAPL=data.current(context.sym, "price"), ma5=ma5, ma20=ma20, buy=buy, sell=sell)
 
 '''
 algo = TradingAlgorithm(initialize=initialize, handle_data=handle_data)
@@ -48,6 +56,18 @@ result = algo.run(data)
 start_utc = start.replace(tzinfo=datetime.timezone.utc)
 end_utc = end.replace(tzinfo=datetime.timezone.utc)
 result = run_algorithm(start=start_utc, end=end_utc, initialize=initialize,
-                       capital_base=10000, handle_data=handle_data, data=data)
+                       capital_base=100000, handle_data=handle_data, data=data)
+'''
+# 포트폴리오 그래프 투자 이익금 확인
+plt.plot(result.index, result.portfolio_value)
+plt.show()
+'''
 
+# 골든 크로스 데드 크로스 표시
+plt.plot(result.index, result.ma5)
+plt.plot(result.index, result.ma20)
+plt.legend(loc='best')
+plt.plot(result.loc[result.buy==True].index, result.ma5[result.buy == True], '^')
+plt.plot(result.loc[result.sell==True].index, result.ma5[result.sell == True], 'v')
 
+plt.show()
